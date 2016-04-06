54d9efadd322a4d625e1c10a624cbaed37516dc3
GNRLies <antoine.pegeot@edu.univ-fcomte.fr>
2016-04-06 15:36:08 +0200

    Commit web

---
+++ Projet2_base_silex_v16_1/Projet2_base_silex/App/config.php
@@ -0,0 +1,8 @@
+<?php
+define("database","apegeot3");
+define("hostname","localhost");
+define("username","apegeot3");
+define("password","1607");
+
+define('BASE_URL', explode('app_dev.php', $_SERVER['SCRIPT_NAME'])[0]);
+//define("BASE_URL","http://votreURL/");
